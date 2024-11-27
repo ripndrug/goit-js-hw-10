9d826911fd5cecef050953953b028b45d2ca27be
Mark <ripndrug@gmail.com>
2024-11-27 16:26:04 +0200

    timer

---
+++ src/js/1-timer.js
@@ -0,0 +1,93 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+
+const flatPicker = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
+const days = document.querySelector('span[data-days]');
+const hours = document.querySelector('span[data-hours]');
+const minutes = document.querySelector('span[data-minutes]');
+const seconds = document.querySelector('span[data-seconds]');
+
+let userSelectedDate;
+let intervalId = null;
+
+startBtn.disabled = true;
+
+startBtn.addEventListener('click', handleStart);
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      console.log(selectedDates[0]);
+      userSelectedDate = selectedDates[0];
+      let currentDate = new Date();
+
+    if (userSelectedDate < currentDate) {
+        alert('Please choose a date in the future');
+        startBtn.disabled = true;
+    } else {
+        startBtn.disabled = false;
+      }
+      
+  },
+};
+
+flatpickr(flatPicker, options);
+
+function handleStart() {
+    startBtn.disabled = true;
+    flatPicker.disabled = true;
+    
+    intervalId = setInterval(() => {
+        const currentDate = new Date();
+        const remainingDateMs = userSelectedDate - currentDate;
+          
+          if (remainingDateMs <= 0) {
+              clearInterval(intervalId);
+              updateTimerUI(0);
+              flatPicker.disabled = false;
+              return;
+          }
+
+          updateTimerUI(remainingDateMs);
+      }, 1000)
+}
+
+function updateTimerUI(ms) {
+    const time = convertMs(ms);
+
+    days.textContent = time.days.toString().padStart(2, '0');
+    hours.textContent = time.hours.toString().padStart(2, '0');
+    minutes.textContent = time.minutes.toString().padStart(2, '0');
+    seconds.textContent = time.seconds.toString().padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+console.log(flatPicker, startBtn, days, hours, minutes, seconds);
